0a75bf3fce1dd9da912aacd3a20d4282d7778656
nixhajete09 <nixhajete05@gmail.com>
2025-04-29 11:31:58 +0200

    init commit

--- app/src/main/java/com/example/teamb_app_prototype/MainActivity.kt
+++ app/src/main/java/com/example/teamb_app_prototype/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.teamb_app_prototype
 
+import androidx.compose.material3.Text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.teamb_app_prototype.ui.theme.TeambappprototypeTheme
+import com.example.teamb_app_prototype.TilfoejScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +19,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TeambappprototypeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "tilføj",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    TilfoejScreen()
                 }
             }
         }
     }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
