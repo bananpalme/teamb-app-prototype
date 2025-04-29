7e65cec68e1180b62771873f9557d41683a32174
bananpalme <nigginz12@gmail.com>
2025-04-29 19:33:54 +0200

    Revert "skiftede gretting tekst"
    This reverts commit aeb9b460f532540239148e1e949da28bbbbffe10.

--- app/src/main/java/com/example/teamb_app_prototype/MainActivity.kt
+++ app/src/main/java/com/example/teamb_app_prototype/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Forside",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
